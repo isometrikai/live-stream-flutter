ce972ce2a040a231f602a5c0b239cad3c798e031
MDTARIQ-appscrip <127086613+MDTARIQ-appscrip@users.noreply.github.com>
2024-02-20 16:05:19 +0530

    feat(stream_header) - ui chnage

--- lib/src/views/stream/widgets/stream_header.dart
+++ lib/src/views/stream/widgets/stream_header.dart
@@ -21,7 +21,6 @@ class StreamHeader extends StatelessWidget {
   Widget build(BuildContext context) => Row(
         mainAxisSize: MainAxisSize.min,
         crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Column(
             mainAxisSize: MainAxisSize.min,
@@ -38,16 +37,68 @@ class StreamHeader extends StatelessWidget {
             ],
           ),
           const Spacer(),
-          IsmLiveTapHandler(
-            onTap: onTapViewers,
-            child: const IsmLiveUsersAvatar(),
-          ),
-          IconButton(
-            icon: const Icon(
-              Icons.close,
-              color: IsmLiveColors.white,
+          GetBuilder<IsmLiveStreamController>(
+            builder: (controller) => IsmLiveTapHandler(
+              onTap: onTapModerators,
+              child: Container(
+                padding: IsmLiveDimens.edgeInsets4,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.white24,
+                ),
+                child: Icon(
+                  Icons.local_police_rounded,
+                  color: IsmLiveColors.white,
+                  size: IsmLiveDimens.sixteen,
+                ),
+              ),
             ),
-            onPressed: onTapCross,
+          ),
+          IsmLiveDimens.boxWidth10,
+          IsmLiveTapHandler(
+            onTap: () => IsmLiveUtility.openBottomSheet(
+              const IsmLiveMembersSheet(),
+              isScrollController: true,
+            ),
+            child: Container(
+              padding: IsmLiveDimens.edgeInsets4,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(IsmLiveDimens.twelve),
+                color: Colors.white24,
+              ),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(
+                    Icons.remove_red_eye,
+                    color: IsmLiveColors.white,
+                    size: IsmLiveDimens.sixteen,
+                  ),
+                  IsmLiveDimens.boxWidth4,
+                  GetX<IsmLiveStreamController>(
+                    builder: (controller) => Text(
+                      controller.streamMembersList.length.toString(),
+                      style: IsmLiveStyles.white12,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+
+          // IsmLiveTapHandler(
+          //   onTap: onTapViewers,
+          //   child: const IsmLiveUsersAvatar(),
+          // ),
+          Container(
+            margin: IsmLiveDimens.edgeInsets10_0,
+            child: IsmLiveTapHandler(
+              onTap: onTapCross,
+              child: const Icon(
+                Icons.close,
+                color: IsmLiveColors.white,
+              ),
+            ),
           ),
         ],
       );
@@ -65,7 +116,9 @@ class _LiveTimer extends StatelessWidget {
         children: [
           DecoratedBox(
             decoration: BoxDecoration(
-              color: Get.find<IsmLiveMqttController>().isConnected ? IsmLiveColors.green : IsmLiveColors.red,
+              color: Get.find<IsmLiveMqttController>().isConnected
+                  ? IsmLiveColors.green
+                  : IsmLiveColors.red,
               borderRadius: BorderRadius.circular(IsmLiveDimens.four),
             ),
             child: Padding(
@@ -89,56 +142,56 @@ class _LiveTimer extends StatelessWidget {
             ),
           ),
           IsmLiveDimens.boxWidth8,
-          IsmLiveTapHandler(
-            onTap: () => IsmLiveUtility.openBottomSheet(
-              const IsmLiveMembersSheet(),
-              isScrollController: true,
-            ),
-            child: Container(
-              padding: IsmLiveDimens.edgeInsets4,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(IsmLiveDimens.eight),
-                color: Colors.black12,
-              ),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Icon(
-                    Icons.person,
-                    color: IsmLiveColors.white,
-                    size: IsmLiveDimens.sixteen,
-                  ),
-                  IsmLiveDimens.boxWidth2,
-                  GetX<IsmLiveStreamController>(
-                    builder: (controller) => Text(
-                      controller.streamMembersList.length.toString(),
-                      style: IsmLiveStyles.white12,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          IsmLiveDimens.boxWidth8,
-          GetBuilder<IsmLiveStreamController>(
-            builder: (controller) => (controller.isMember || (controller.isCopublisher ?? false))
-                ? IsmLiveTapHandler(
-                    onTap: onTapModerators,
-                    child: Container(
-                      padding: IsmLiveDimens.edgeInsets4,
-                      decoration: const BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Colors.black12,
-                      ),
-                      child: Icon(
-                        Icons.local_police_rounded,
-                        color: IsmLiveColors.white,
-                        size: IsmLiveDimens.sixteen,
-                      ),
-                    ),
-                  )
-                : IsmLiveDimens.box0,
-          ),
+          // IsmLiveTapHandler(
+          //   onTap: () => IsmLiveUtility.openBottomSheet(
+          //     const IsmLiveMembersSheet(),
+          //     isScrollController: true,
+          //   ),
+          //   child: Container(
+          //     padding: IsmLiveDimens.edgeInsets4,
+          //     decoration: BoxDecoration(
+          //       borderRadius: BorderRadius.circular(IsmLiveDimens.eight),
+          //       color: Colors.black12,
+          //     ),
+          //     child: Row(
+          //       mainAxisSize: MainAxisSize.min,
+          //       children: [
+          //         Icon(
+          //           Icons.person,
+          //           color: IsmLiveColors.white,
+          //           size: IsmLiveDimens.sixteen,
+          //         ),
+          //         IsmLiveDimens.boxWidth2,
+          //         GetX<IsmLiveStreamController>(
+          //           builder: (controller) => Text(
+          //             controller.streamMembersList.length.toString(),
+          //             style: IsmLiveStyles.white12,
+          //           ),
+          //         ),
+          //       ],
+          //     ),
+          //   ),
+          // ),
+          // IsmLiveDimens.boxWidth8,
+          // GetBuilder<IsmLiveStreamController>(
+          //   builder: (controller) => (controller.isMember || (controller.isCopublisher ?? false))
+          //       ? IsmLiveTapHandler(
+          //           onTap: onTapModerators,
+          //           child: Container(
+          //             padding: IsmLiveDimens.edgeInsets4,
+          //             decoration: const BoxDecoration(
+          //               shape: BoxShape.circle,
+          //               color: Colors.black12,
+          //             ),
+          //             child: Icon(
+          //               Icons.local_police_rounded,
+          //               color: IsmLiveColors.white,
+          //               size: IsmLiveDimens.sixteen,
+          //             ),
+          //           ),
+          //         )
+          //       : IsmLiveDimens.box0,
+          // ),
         ],
       );
 }
