40ad5e4604fcc5c3d5f5d564ac8d2845c4c1e13e
MDTARIQ-appscrip <127086613+MDTARIQ-appscrip@users.noreply.github.com>
2024-06-10 19:35:23 +0530

    bug fix

--- lib/src/views/stream/stream_view.dart
+++ lib/src/views/stream/stream_view.dart
@@ -256,7 +256,8 @@ class _IsmLiveStreamView extends StatelessWidget {
                 ],
                 if (controller.isPk &&
                     !(controller.pkStages?.isPkStart ?? false) &&
-                    !(controller.userRole?.isViewer ?? false) &&
+                    (controller.userRole?.isPkGuest ?? false) &&
+                    (controller.userRole?.isHost ?? false) &&
                     !controller.animationController.isCompleted &&
                     controller.participantTracks.length == 2) ...[
                   AnimatedBuilder(
