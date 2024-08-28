98296d3893734285084dadb111598a5d5f315e14
MDTARIQ-appscrip <127086613+MDTARIQ-appscrip@users.noreply.github.com>
2024-08-28 16:25:29 +0530

    customize end stream ui

--- lib/src/live_app.dart
+++ lib/src/live_app.dart
@@ -92,6 +92,7 @@ class IsmLiveApp extends StatelessWidget {
     List<IsmLiveStreamOption> copublisherOptions = const [],
     List<IsmLiveStreamOption> pkOptions = const [],
     Widget? homeScreen,
+    Widget? endStreamScreen,
   }) {
     assert(_initialized,
         'IsmLiveApp is not initialized, initialize it using `IsmLiveApp.initialize()`');
@@ -108,6 +109,7 @@ class IsmLiveApp extends StatelessWidget {
     IsmLiveDelegate.copublisherOptions = copublisherOptions;
     IsmLiveDelegate.pkOptions = pkOptions;
     IsmLiveDelegate.homeScreen = homeScreen;
+    IsmLiveDelegate.endStreamScreen = endStreamScreen;
     IsmLiveDelegate.scheduleStream = scheduleStream;
     IsmLiveDelegate.hdStream = hdStream;
     IsmLiveDelegate.paidStream = paidStream;
--- lib/src/views/stream/views/end_stream_view.dart
+++ lib/src/views/stream/views/end_stream_view.dart
@@ -21,103 +21,107 @@ class IsmLiveEndStream extends StatelessWidget {
                 ..streamAnalyticsViewers(streamId: streamId);
             });
           },
-          builder: (controller) => Scaffold(
-            body: Padding(
-              padding: IsmLiveDimens.edgeInsets8,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  IsmLiveDimens.boxHeight32,
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          builder: (controller) =>
+              IsmLiveDelegate.endStreamScreen ??
+              Scaffold(
+                body: Padding(
+                  padding: IsmLiveDimens.edgeInsets8,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      IsmLiveDimens.boxWidth50,
-                      IsmLiveImage.network(
-                        controller.user?.userProfileImageUrl ?? '',
-                        name: controller.user?.userName ?? 'U',
-                        height: IsmLiveDimens.ninty,
-                        width: IsmLiveDimens.ninty,
-                        isProfileImage: true,
+                      IsmLiveDimens.boxHeight32,
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          IsmLiveDimens.boxWidth50,
+                          IsmLiveImage.network(
+                            controller.user?.userProfileImageUrl ?? '',
+                            name: controller.user?.userName ?? 'U',
+                            height: IsmLiveDimens.ninty,
+                            width: IsmLiveDimens.ninty,
+                            isProfileImage: true,
+                          ),
+                          IconButton(
+                            icon: const Icon(
+                              Icons.close,
+                            ),
+                            onPressed: Get.back,
+                          ),
+                        ],
                       ),
-                      IconButton(
-                        icon: const Icon(
-                          Icons.close,
+                      IsmLiveDimens.boxHeight16,
+                      Text(
+                        'Live stream ended!',
+                        style: context.textTheme.titleMedium?.copyWith(
+                          fontWeight: FontWeight.bold,
                         ),
-                        onPressed: Get.back,
                       ),
+                      IsmLiveDimens.boxHeight16,
+                      const Divider(
+                        thickness: 0.5,
+                      ),
+                      GridView(
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 3,
+                          childAspectRatio: 1.5,
+                        ),
+                        children: [
+                          IsmLiveEndStreamContainer(
+                            points: '${controller.streamAnalytis?.hearts ?? 0}',
+                            title: 'Hearts',
+                            color: Colors.black,
+                            assetConstant: IsmLiveAssetConstants.heartSvg,
+                          ),
+                          IsmLiveEndStreamContainer(
+                            points:
+                                '${controller.streamAnalytis?.productCount ?? 0}',
+                            title: 'Order',
+                            assetConstant: IsmLiveAssetConstants.box,
+                          ),
+                          IsmLiveEndStreamContainer(
+                            points:
+                                '${controller.streamAnalytis?.totalViewersCount ?? 0}',
+                            title: 'Viewers',
+                            assetConstant: IsmLiveAssetConstants.eye,
+                          ),
+                          IsmLiveEndStreamContainer(
+                            points:
+                                '${controller.streamAnalytis?.followers ?? 0}',
+                            title: 'Followers',
+                            assetConstant: IsmLiveAssetConstants.profileUser,
+                          ),
+                          IsmLiveEndStreamContainer(
+                            points:
+                                '\$${controller.streamAnalytis?.totalEarning ?? 0}',
+                            title: 'Earnings',
+                            assetConstant: IsmLiveAssetConstants.dollar,
+                          ),
+                          IsmLiveEndStreamContainer(
+                            points:
+                                '${controller.streamAnalytis?.formattedDuration}',
+                            title: 'Duration',
+                            assetConstant: IsmLiveAssetConstants.clock,
+                          ),
+                        ],
+                      ),
+                      Divider(
+                        thickness: 5,
+                        color: Colors.grey.shade300,
+                      ),
+                      Obx(() => IsmLiveListSheetTwo(
+                            scrollController: controller.viewerListController,
+                            items: controller.analyticsViewers,
+                            title:
+                                'Viewer(${controller.analyticsViewers.length})',
+                          )),
                     ],
                   ),
-                  IsmLiveDimens.boxHeight16,
-                  Text(
-                    'Live stream ended!',
-                    style: context.textTheme.titleMedium?.copyWith(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  IsmLiveDimens.boxHeight16,
-                  const Divider(
-                    thickness: 0.5,
-                  ),
-                  GridView(
-                    shrinkWrap: true,
-                    physics: const NeverScrollableScrollPhysics(),
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 3,
-                      childAspectRatio: 1.5,
-                    ),
-                    children: [
-                      IsmLiveEndStreamContainer(
-                        points: '${controller.streamAnalytis?.hearts ?? 0}',
-                        title: 'Hearts',
-                        color: Colors.black,
-                        assetConstant: IsmLiveAssetConstants.heartSvg,
-                      ),
-                      IsmLiveEndStreamContainer(
-                        points:
-                            '${controller.streamAnalytis?.productCount ?? 0}',
-                        title: 'Order',
-                        assetConstant: IsmLiveAssetConstants.box,
-                      ),
-                      IsmLiveEndStreamContainer(
-                        points:
-                            '${controller.streamAnalytis?.totalViewersCount ?? 0}',
-                        title: 'Viewers',
-                        assetConstant: IsmLiveAssetConstants.eye,
-                      ),
-                      IsmLiveEndStreamContainer(
-                        points: '${controller.streamAnalytis?.followers ?? 0}',
-                        title: 'Followers',
-                        assetConstant: IsmLiveAssetConstants.profileUser,
-                      ),
-                      IsmLiveEndStreamContainer(
-                        points:
-                            '\$${controller.streamAnalytis?.totalEarning ?? 0}',
-                        title: 'Earnings',
-                        assetConstant: IsmLiveAssetConstants.dollar,
-                      ),
-                      IsmLiveEndStreamContainer(
-                        points:
-                            '${controller.streamAnalytis?.formattedDuration}',
-                        title: 'Duration',
-                        assetConstant: IsmLiveAssetConstants.clock,
-                      ),
-                    ],
-                  ),
-                  Divider(
-                    thickness: 5,
-                    color: Colors.grey.shade300,
-                  ),
-                  Obx(() => IsmLiveListSheetTwo(
-                        scrollController: controller.viewerListController,
-                        items: controller.analyticsViewers,
-                        title: 'Viewer(${controller.analyticsViewers.length})',
-                      )),
-                ],
+                ),
               ),
-            ),
-          ),
         ),
       );
 }
