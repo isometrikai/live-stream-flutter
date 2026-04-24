5ca8d1be309ff4bd89d1adfcde475c09b6c3bdb1
mayurappscripde <146091920+mayurappscripde@users.noreply.github.com>
2026-04-24 17:32:16 +0530

    Fix: Double EndStream screen opening

--- lib/src/views/stream/widgets/stream_card.dart
+++ lib/src/views/stream/widgets/stream_card.dart
@@ -72,8 +72,10 @@ class IsmLiveStreamCard extends StatelessWidget {
                                   ? stream.scheduleStartTime!.formattedDate
                                   : IsmLiveStrings.tvContinue,
                               style: context.textTheme.labelSmall?.copyWith(
-                                color: Theme.of(context).brightness == Brightness.dark
-                                    ? context.liveTheme?.primaryButtonTheme?.foregroundColor ??
+                                color: Theme.of(context).brightness ==
+                                        Brightness.dark
+                                    ? context.liveTheme?.primaryButtonTheme
+                                            ?.foregroundColor ??
                                         IsmLiveColors.black
                                     : context.liveTheme?.selectedTextColor ??
                                         IsmLiveColors.white,
@@ -92,7 +94,7 @@ class IsmLiveStreamCard extends StatelessWidget {
                       children: [
                         IsmLiveImage.network(
                           stream.userDetails?.userProfile ?? '',
-                          name: stream.userDetails?.userName ?? 'U',
+                          name: stream.userDetails?.name ?? 'U',
                           dimensions: IsmLiveDimens.thirtyTwo,
                           isProfileImage: true,
                         ),
