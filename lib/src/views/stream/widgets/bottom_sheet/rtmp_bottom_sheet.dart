162633fb580cd41fd06131b1cec5c97cf9509574
mayurappscripde <146091920+mayurappscripde@users.noreply.github.com>
2026-05-29 15:41:00 +0530

    Feat: 'IsmLiveRtmpSheet' UI update

--- lib/src/views/stream/widgets/bottom_sheet/rtmp_bottom_sheet.dart
+++ lib/src/views/stream/widgets/bottom_sheet/rtmp_bottom_sheet.dart
@@ -6,15 +6,29 @@ import 'package:get/get.dart';
 class IsmLiveRtmpSheet extends StatelessWidget {
   const IsmLiveRtmpSheet();
 
+  static Color _textOnBackground(Color background) =>
+      ThemeData.estimateBrightnessForColor(background) == Brightness.dark
+          ? Colors.white
+          : Colors.black;
+
+  static Color _subtitleOnBackground(Color background) {
+    final onDark =
+        ThemeData.estimateBrightnessForColor(background) == Brightness.dark;
+    return onDark ? const Color(0xFFB0B0B0) : (Colors.grey[600] ?? Colors.grey);
+  }
+
   @override
   Widget build(BuildContext context) {
     final isDarkMode = Theme.of(context).brightness == Brightness.dark;
-    final bgColor = context.liveTheme?.backgroundColor ??
+    final sheetBackground = context.liveTheme?.backgroundColor ??
         (isDarkMode ? const Color(0xFF121212) : Colors.white);
+    final textColor = _textOnBackground(sheetBackground);
+    final subtitleColor = context.liveTheme?.unselectedTextColor ??
+        _subtitleOnBackground(sheetBackground);
 
     return Container(
       decoration: BoxDecoration(
-        color: bgColor,
+        color: sheetBackground,
         borderRadius: BorderRadius.vertical(
           top: Radius.circular(
             IsmLiveDelegate.bottomSheetBorderRadius?.topLeft.x ??
@@ -34,6 +48,8 @@ class IsmLiveRtmpSheet extends StatelessWidget {
                 label: IsmLiveStrings.rtmlUrl,
                 readOnly: true,
                 controller: controller.rtmlUrlDevice,
+                sheetBackground: sheetBackground,
+                textColor: textColor,
                 onTap: () {
                   Clipboard.setData(
                     ClipboardData(text: controller.rtmlUrlDevice.text),
@@ -46,6 +62,8 @@ class IsmLiveRtmpSheet extends StatelessWidget {
                 hint: IsmLiveStrings.streamKeyHint,
                 readOnly: true,
                 controller: controller.streamKeyDevice,
+                sheetBackground: sheetBackground,
+                textColor: textColor,
                 onTap: () {
                   Clipboard.setData(
                     ClipboardData(text: controller.streamKeyDevice.text),
@@ -58,8 +76,7 @@ class IsmLiveRtmpSheet extends StatelessWidget {
                   text: IsmLiveStrings.rtmpStreamInstruction,
                 ),
                 style: context.textTheme.labelMedium?.copyWith(
-                  color: context.liveTheme?.primaryColor ??
-                      (isDarkMode ? Colors.white : Colors.black),
+                  color: subtitleColor,
                 ),
               )
             ],
@@ -73,6 +90,8 @@ class IsmLiveRtmpSheet extends StatelessWidget {
 class _InputField extends StatelessWidget {
   const _InputField({
     required this.label,
+    required this.sheetBackground,
+    required this.textColor,
     this.hint,
     required this.controller,
     this.readOnly = false,
@@ -80,6 +99,8 @@ class _InputField extends StatelessWidget {
   });
 
   final String label;
+  final Color sheetBackground;
+  final Color textColor;
   final String? hint;
   final TextEditingController controller;
   final bool readOnly;
@@ -87,15 +108,15 @@ class _InputField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final isDarkMode = Theme.of(context).brightness == Brightness.dark;
-    final textColor = context.liveTheme?.primaryColor ??
-        (isDarkMode ? Colors.white : Colors.black);
+    final onDarkSheet =
+        ThemeData.estimateBrightnessForColor(sheetBackground) ==
+            Brightness.dark;
     final borderColor = context.liveTheme?.borderColor ??
-        (isDarkMode ? const Color(0xFF1E1E1E) : IsmLiveColors.black);
+        (onDarkSheet ? const Color(0xFF1E1E1E) : IsmLiveColors.black);
     final fillColor = context.liveTheme?.cardBackgroundColor ??
-        (isDarkMode ? const Color(0xFF1E1E1E) : Colors.white);
-    final iconColor = context.liveTheme?.primaryColor ??
-        (isDarkMode ? Colors.white : Colors.black);
+        (onDarkSheet ? const Color(0xFF1E1E1E) : Colors.white);
+    final hintColor = context.liveTheme?.unselectedTextColor ??
+        IsmLiveRtmpSheet._subtitleOnBackground(sheetBackground);
 
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
@@ -111,8 +132,7 @@ class _InputField extends StatelessWidget {
           controller: controller,
           hintText: hint ?? 'Enter $label',
           hintStyle: context.textTheme.labelLarge?.copyWith(
-            color: context.liveTheme?.unselectedTextColor ??
-                (isDarkMode ? const Color(0xFFB0B0B0) : Colors.grey),
+            color: hintColor,
           ),
           style: context.textTheme.labelLarge?.copyWith(
             color: textColor,
@@ -124,7 +144,7 @@ class _InputField extends StatelessWidget {
           borderColor: borderColor,
           suffixIcon: Icon(
             Icons.copy,
-            color: iconColor,
+            color: textColor,
           ),
         ),
       ],
